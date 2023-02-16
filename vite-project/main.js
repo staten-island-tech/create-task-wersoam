f5462c6b498304433268e83f549395ee8f301b90
wersoam <cowistrash@gmail.com>
2023-02-16 10:16:19 -0500

    commit

--- vite-project/main.js
+++ vite-project/main.js
@@ -3,6 +3,7 @@ import { array } from "./array";
 
 document.querySelector("#app").innerHTML = `
   <div>
-    ${array[11].picture}
+  <img src="${array[1].picture}" alt=""> <h2>${array[1].name}</h2>
+  <p>From the country of japan arrives series that would be great kids T.V shows, if only they officially translate it</p>
   </div>
 `;
